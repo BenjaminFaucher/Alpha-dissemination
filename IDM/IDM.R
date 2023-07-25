2e5f8a5c760a81edf85ce31bde9320fbcc431a30
BenjaminFaucher <135997976+BenjaminFaucher@users.noreply.github.com>
2023-07-25 12:19:11 +0200

    Update IDM.R

--- IDM/IDM.R
+++ IDM/IDM.R
@@ -4,16 +4,25 @@ source("IDM_functions.R")
 packageVersion("importFromUK") # should be 3.2
 ################################
 # Import input data
-load("inputs_IDM.RData")
-load("delays.RData") #sym_agg
+load("Inputs.RData")
+
+date.ref = as.Date("2020-08-15")
+date.thr = as.Date("2020-12-31")
+inherits = FALSE
+T0.max = 38
+T0.start = 32
+Tc2.LD = 83
+Tc3.LD = 126
+
+#load("smallStart.RData")
 ################################
 
-# Make data 
-data = make.data.dynamic(first.VOC=first.VOC.submitted,screen=screening.1,travel=travel.london.choc,epidUK=epid.UK2,dt.start.epid = as.Date("2020-10-01"),
-dt.stop.epid = as.Date("2020-12-31"),date.thr = as.Date("2020-12-31"),pop.uk = 36e6, 
-R=R.VOC.smooth,r=r.VOC.smooth,R.increase=0.5, K=0.5, K.UK=0.25,s.increase=1,dt.s.increase = as.Date("2020-12-18"),
-incub = 5,delai.sub1 = delai.sub.by.country.1to3$delai.1,delai.sub2 = sym_agg$delai.1,date.change=as.Date("2020-12-18"),
-ahead=7,f_before_T0=1.0)
+# Make data
+data = make.data.dynamic(first.VOC=first.VOC.fake,screen=screening.fake,travel=travel.london.fake,epidUK=epid.UK.fake,dt.start.epid = as.Date("2020-10-01"),
+                         dt.stop.epid = as.Date("2020-12-31"),date.thr = as.Date("2020-12-31"),pop.uk = 36e6, 
+                         R=R.VOC.smooth.fake,r=r.VOC.smooth.fake,R.increase=0.5, K=0.5, K.UK=0.25,s.increase=1,dt.s.increase = as.Date("2020-12-18"),
+                         incub = 5,delai.sub1 = delaysbefore$delai.1,delai.sub2 = delaysafter$delai.1,date.change=as.Date("2020-12-18"),
+                         ahead=7,f_before_T0=1.0)
 
 ################################
 
@@ -28,11 +37,10 @@ res= MCMC.import.cpp(param = c("r","T0","r2","s"),ll="pois",llEpid = NULL,
 sum = summary.MCMC.import(res,data)
 sum.count = summary.countries(sum,data)
 
+
 ################################
 
 
-# Save results files
-save(res,file = "./sensitivity/res_baseline.RData")
-save(sum,file = "./sensitivity/sum_baseline.RData")
-save(sum.count,file = "./sensitivity/sumcount_baseline.RData")
+
+
 
