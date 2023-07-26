f323926c3d19186ec473cfd91bbfcb2033a1335e
BenjaminFaucher <135997976+BenjaminFaucher@users.noreply.github.com>
2023-07-26 10:11:56 +0200

    Update readDataGISAID-CSSE.R

--- Data Processing/readDataGISAID-CSSE.R
+++ Data Processing/readDataGISAID-CSSE.R
@@ -575,6 +575,7 @@ return(list(screening.30=screening.30, screening.15=screening.15, screening.7=sc
 }
 
 screening.all.sub31 = compute.seq.fraction(sequences.all.col.sub31, CSSE.all,date.end=as.Date("2020-12-31"))
+screening <- screening.all.sub31$screening.1
 
 plot(seq(screening.all.sub31$date.start, screening.all.sub31$date.end,1),screening.all.sub31$screening.30["France",],t='l')
 
@@ -729,7 +730,131 @@ compute.delai <- function(metadata, date.start=as.Date("2020-08-01"), date.end=a
   delai.sub.by.country
 }
 
-delai.sub.by.country.1to3=compute.delai(metadata)
+delaysbefore=compute.delai(metadata)
 
 delai.sub.by.country.VOC=compute.delai(metadata[metadata$Pango.lineage=="B.1.1.7",])
+
+
+
+############################
+
+## Matrices of delays #####
+
+############################
+#Import sequences of UK variants outside of UK
+
+GISAID = metadata
+GISAID <- GISAID[GISAID$alpha == 1,] 
+df = GISAID[GISAID$country != "United Kingdom",]
+df$date.col = as.Date(df$date.col)
+df$date.sub = as.Date(df$date.sub)
+############################
+
+#Compute the matrice M where M[i,j] = n(i,j)/N(i) where N(i) is the number of collected sequences between day i-1 and i+1
+#and n(i,j) is the number of these sequences that are submitted day j.
+
+df <- df %>%
+  arrange(date.col, date.sub)
+
+df$date.col <- as.Date(df$date.col, format = "%m-%d-%Y")
+
+first_date <- as.Date("2020-01-01", format = "%Y-%m-%d")
+df$col1 <- as.numeric(df$date.col - first_date) + 1
+df$col2 <- df$col1 + 1
+df$col3 <- df$col1 - 1
+
+df$sub1 <- df$col1 + df$delai
+df$sub2 <- df$col2 + df$delai
+df$sub3 <- df$col3 + df$delai
+
+col <- c(df$col1, df$col2, df$col3)
+sub <- c(df$sub1, df$sub2, df$sub3)
+dd <- data.frame(x = col, y = sub)
+
+count_df <- dd %>%
+  group_by(x, y) %>%
+  summarise(count = n())
+
+total_df <- dd %>%
+  group_by(x) %>%
+  summarise(total = n())
+
+merged_df <- left_join(count_df, total_df, by = "x")
+
+size <- 519
+M <- matrix(0, nrow = size, ncol = size)
+
+for (i in 1:nrow(merged_df)) {
+  x <- merged_df$x[i]
+  y <- merged_df$y[i]
+  count <- merged_df$count[i]
+  total <- merged_df$total[i]
+  M[x, y] <- count / total
+}
+rownames(M) <- paste0("d.", 1:nrow(M))
+colnames(M) <- paste0("d.", 1:ncol(M))
+
+delaysafter <- M
+
+############################
+#Store the dataframein a list for each country
+
+nn <- c("Afghanistan", "Albania", "Algeria", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+        "Belize", "Bermuda", "Bolivia", "Bonaire", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands",
+        "Brunei", "Bulgaria","Burkina Faso", "Cambodia", "Cameroon", "Canada", "Cayman Islands", "Central African Republic", "Chile", "China", "Chinese Taipei", "Colombia", "Comoros", "Costa Rica", "Cote d\'Ivoire", "Crimea", "Croatia",
+        "Cuba", "Curacao", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Eswatini", "Ethiopia", "Faroe Islands", "Finland", 
+        "France", "French Guiana", "French Polynesia", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea Bissau", "Guinea", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", 
+        "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kosovo", "Kuwait", "Latvia", "Lebanon", "Lesotho", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", 
+        "Malawi", "Malaysia", "Malta", "Martinique", "Mauritius", "Mayotte", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nepal", "Netherlands", "New Zealand", "Niger", "Nigeria", "Northern Mariana Islands",
+        "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Republic of the Congo", "Reunion", "Romania", "Russian Federation", "Rwanda","Saint Lucia", "Saint Vincent and Grenadines",
+        "Saudi Arabia", "Senegal", "Serbia", "Sierra Leone", "Singapore", "Sint Eustatius", "Sint Maarten", "Slovakia", "Slovenia", "Somalia", "South Africa", "South Korea", "Spain", "Sri Lanka", "Suriname", "Sweden", "Switzerland", "Thailand", "Togo", "Trinidad and Tobago",
+        "Tunisia", "Turkey", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Zambia", "Zimbabwe")
+
+
+delaysafter <- list()
+
+# Boucle pour créer les copies de dataframe avec les noms souhaités et les stocker dans la liste
+for (i in seq_along(nn)) {
+  delaysafter[[i]] <- data.frame(M)  # Créer une copie de dataframe (pour éviter les liens)
+  names(delaysafter)[i] <- nn[i]       # Attribuer le nom du vecteur 'v' à chaque dataframe dans la liste
+}
+
+##########################
+# Computing first date of submission and collection date associated
+###########################
+
+countries = c("United Kingdom","Denmark","Australia","Gibraltar","Italy","Netherlands","Singapore","Hong Kong","Israel","Ireland","Japan",
+              "France","Spain","Portugal","Norway","Finland","Sweden","South Korea","Canada","India","Germany","Switzerland","United States of America",
+              "Brazil","New Zealand","Luxembourg","Pakistan","Belgium","Slovakia","Mexico","Sri Lanka","Romania","Ecuador","Turkey","Hungary","Peru","Iran",
+              "Poland","Czech Republic","Austria","Latvia","United Arab Emirates","Trinidad and Tobago","Nigeria","Jordan","Malaysia","Bangladesh",            
+              "Thailand","South Africa","Gambia","Ghana","Macedonia","Bosnia and Herzegovina","Chinese Taipei","Russian Federation","Georgia","Rwanda",
+              "Lithuania","Croatia","Indonesia","China","Chile","Tunisia","Belarus","Cote d'Ivoire","Palestine","Colombia","Bahrain","Malta","Burkina Faso",           
+              "Faroe Islands","Uganda","El Salvador","Madagascar","Papua New Guinea")
+
+GISAID = metadata
+GISAID <- GISAID[GISAID$alpha == 1,] 
+#GISAID$date.sub <- as.Date(GISAID$date.sub)
+#GISAID <- GISAID[GISAID$country %in% countries,] %>% group_by(country) %>% summarise(date.submission = min(date.sub),date.collection = date.col[which(date.sub == min(date.sub))], date.collection.lo = date.col.min[which(date.sub == min(date.sub))], date.collection.up = date.col.max[which(date.sub == min(date.sub))], col.imputed = imputed[which(date.sub == min(date.sub))], date.1st.sub = min(date.sub))
+
+idx <- GISAID %>% group_by(country) %>% slice(which.min(as.Date(date.sub)))
+idx <- idx[idx$country %in% countries,]
+first.VOC = data.frame(country=idx$country,date.collection=idx$date.col,date.submission = idx$date.sub,col.imputed = idx$imputed, date.1st.sub=idx$date.sub , date.collection.lo = idx$date.col.min, date.collection.up = idx$date.col.max)
+df <- data.frame(country=c("El Salvador","Madagascar","Papua New Guinea"),date.collection=rep(NA,3),date.submission = rep(NA,3),col.imputed = rep(NA,3), date.1st.sub=rep(NA,3) , date.collection.lo = rep(NA,3), date.collection.up = rep(NA,3))
+first.VOC <- rbind(df,first.VOC)
+rownames(first.VOC) =first.VOCf$country
+
+
+first.VOC$date.collection <- as.Date(first.VOC$date.collection)
+first.VOC$date.submission <- as.Date(first.VOC$date.submission)
+first.VOC$date.1st.sub <- as.Date(first.VOC$date.1st.sub)
+first.VOC$date.collection.lo <- as.Date(first.VOC$date.collection.lo)
+first.VOC$date.collection.up <- as.Date(first.VOC$date.collection.up)
+
+save(
+      list = c("delaysafter","delaysbefore","first.VOC","screening"),
+         file = "./Inputs_IDM.RData"
+    )
+
+
+
 
